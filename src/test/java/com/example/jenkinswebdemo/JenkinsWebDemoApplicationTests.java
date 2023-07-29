290bee4ed1ce80c9a0e9642be6cda68f3d7e33c7
ImYoungGwang <aas9429@naver.com>
2023-07-30 03:09:41 +0900

    first commit

---
+++ src/test/java/com/example/jenkinswebdemo/JenkinsWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinswebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
